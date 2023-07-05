f64cebaa82fe8b86b798a210d9743e8a45935bda
hp <hp@DESKTOP-2VE9LHI>
2023-07-05 19:54:24 +0400

    Add project files.

---
+++ class_exercises-6/Program.cs
@@ -0,0 +1,75 @@
+﻿
+/*class student
+{
+    string name = "nukri";
+    string lastname = "tsaava";
+    int GPA = 4;
+
+    static void Main(string[] args)
+    {
+        student myObj = new student();
+        Console.WriteLine(myObj.name);
+        Console.WriteLine(myObj.lastname);
+        Console.WriteLine(myObj.GPA);
+    }
+}
+*/
+/*მარტკუტხედის ფარტობი*/
+
+/*
+public class martkutxedis_fartobi
+{
+    public static void Main()
+    {
+        Console.WriteLine("sheiyvanet martkutxedis sigrze : ");
+        int sigrze = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("sheiyvanet markutxedis sigane: ");
+        int sigane = Convert.ToInt32(Console.ReadLine());
+        int fartobi = (sigrze * sigane);
+        Console.WriteLine($"martkutxedis sigrze {sigrze} martkutxedis sigane {sigane} martkutxedis  fartobi = {fartobi}");
+        Console.ReadKey();
+    }
+}
+
+*/
+
+
+/*სამკუთხესი ფართობის გამოთვლა: (გვერდის სიგრძე * სიმაღლეზე)/ 2 -ზე.*/
+
+/*
+
+public class samkutxedisfartobi
+{
+    public static void Main()
+    {
+        Console.WriteLine("sheiyvanet samkutxedis sigrze : ");
+        int sigrze = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("sheiyvanet samkutxedis simagle: ");
+        int simagle = Convert.ToInt32(Console.ReadLine());
+        int fartobi = (sigrze * simagle)/2;
+        Console.WriteLine($"samkutxedis sigrze {sigrze} samkutxedis simagle {simagle} samkutxedis fartobi {fartobi}");
+        Console.ReadKey();
+    }
+}
+*/
+
+
+/*სამკუთხედის პერიმეტრი, სამი გვერდი ჯამი */
+/*
+public class samkutxedisperimetri
+{
+    public static void Main()
+    {
+        Console.WriteLine("sheiyvanet samkutxedis pirveli gverdi : ");
+        int gverdi_1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("sheiyvanet samkutxedis meore gverdi: ");
+        int gverdi_2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("sheiyvanet samkutxedis mesame gverdi");
+        int gverdi_3 = Convert.ToInt32(Console.ReadLine());
+        int perimetri = (gverdi_1 + gverdi_2+gverdi_3) ;
+        Console.WriteLine($"samkutxedis pirveli gverdi : {gverdi_1} samkutxedis meore gverdi : {gverdi_2} samkutxedis mesame gverdi: {gverdi_3} samkutxedis perimetri :  {perimetri} ");
+        Console.ReadKey();
+    }
+}
+
+*/
